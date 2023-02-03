d6f41b113cc96adab37b902cd7798f525d758dcc
LadyNick <72218995+LadyNick@users.noreply.github.com>
2023-02-03 00:48:34 +0000

    runs without errors right now

--- ripple_carry_adder.v
+++ ripple_carry_adder.v
@@ -27,27 +27,28 @@ module ripple_carry_adder # ( parameter NUMBITS = 16 ) (
     // ------------------------------
     // Insert your solution below
     // ------------------------------ 
+    output wire [NUMBITS-1:0] result_ripple;
+    output wire [NUMBITS-1:0] carryout_ripple;
 
-
-    full_adder #(.NUMBITS(1)) first(  .A(A[0]),
-                                        .B(B[0]),
-                                        .c_in(carryin),
-                                        .s(result[0])
-                                        .c_out(carryout));
+    full_adder #() first(  .a(A[0]),
+                            .b(B[0]),
+                            .c_in(carryin),
+                            .s(result_ripple[0]),
+                            .c_out(carryout_ripple[0]));
 
     genvar i;
     generate
-        for(i=2; i<NUMBITS; i = i+1)begin
-            full_adder #(.NUMBITS(i)) ripplecont(  .A(A[i]),
-                                                .B(B[i]),
-                                                .c_in(carryout),
-                                                .s(result[i])
-                                                .c_out(carryin));
-            
+        for(i=1; i<NUMBITS; i = i+1)begin
+            full_adder #() ripplecont(  .a(A[i]),
+                                        .b(B[i]),
+                                        .c_in(carryout_ripple[i-1]),
+                                        .s(result_ripple[i]),
+                                        .c_out(carryout_ripple[i]));  
         end
     endgenerate
 
-
-
+    always @(*) begin
+        result <= result_ripple;
+    end
 
 endmodule
